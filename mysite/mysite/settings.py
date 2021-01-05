93076a99142b2535cfc38c48644295d2b9a097fa
thetechnocrat <thetechnocrat.tech@gmail.com>
2021-01-05 13:58:01 +0530

    made changes for deployment in heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '32jgg@052k^8y#nh7x0+*lffj#4j+2l^6g2cjy)6rk^onile3z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','thetechnocratblog.herokuapp.com']
 
 
 # Application definition
